34093e06eb8f04ee1c0d837167edfaf2975982bb
Anish Chaulagain <anishchaulagain2058@gmail.com>
2026-02-08 21:37:56 +0545

    fix abort error warning

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -15,7 +15,9 @@ export default function VideoPlayer({ videoUrl, poster }: VideoPlayerProps) {
 
     if (videoElement) {
       videoElement.play().catch((error) => {
-        console.error("Autoplay failed:", error);
+        if (error.name !== "AbortError") {
+          console.error("Autoplay failed:", error);
+        }
       });
     }
 
